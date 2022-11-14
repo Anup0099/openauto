8d9903dc1058760ce9dd44791570abd4f7770d5f
anup <anuppal602@gmail.com>
2022-11-14 12:58:24 +0530

    font

--- tailwind.config.js
+++ tailwind.config.js
@@ -19,7 +19,7 @@ module.exports = {
     },
     fontFamily: {
       inter: " 'Inter',sans-serif ",
-      public: " 'Inter',sans-serif ",
+      public: " 'Public Sans',sans-serif ",
     },
     screens: {
       sm: { max: '640px' },
